dfd665984d2c1d463d6600ad61423b3ff7540d2e
GGerginov <electro44@abv.bg>
2022-12-04 20:40:03 +0200

    Pokedex

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 import "../scss/app.scss";
+import {data} from "autoprefixer";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded",function(){
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+      .then(response => response.json())
+      .then(data =>data.results)
+      .then(data => {
+        for (let pokemon of data) {
+          let li = document.createElement('li');
+          li.innerHTML = pokemon.name
+          ul.appendChild(li);
+        }
+      })
 });
+
